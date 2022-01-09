00a657186bb07f423543922d8235f9b952f80f89
thejakgithub <jak71646167@gmail.com>
2022-01-09 17:17:01 +0700

    update

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -24,9 +24,9 @@ export default function Content() {
             <div key={port.id} className="col-lg-4 col-md-6 ">
               <div className="text-center  text-md-start   mx-1 mx-lg-0">
                 <div className="wrapper-img  ">
-                  <div className="img-hover    ">
+                  <div className="img-hover ">
                     <LazyLoadImage
-                      className="img-fluid   px-lg-0 pe-0 pe-md-2 "
+                      className="img-fluid    px-lg-0 pe-0 pe-md-2 "
                       alt={port.img}
                       src={port.img}
                       effect="blur"
@@ -35,47 +35,49 @@ export default function Content() {
                   </div>
                   {isLoading === false && (
                     <LazyLoadImage
-                      className="img-fluid  project-hover  px-lg-0 pe-0 pe-md-2 "
+                      className="img-fluid    project-hover  px-lg-0 pe-0 pe-md-2 "
                       alt="https://wolvescorp.com/main/portfolio/img/project_hover.svg"
                       src="https://wolvescorp.com/main/portfolio/img/project_hover.svg"
                     />
                   )}
                 </div>
 
-                <h5 className="font-poppins portfoio-title my-3">
-                  {port.title}
-                </h5>
-                {port.type.map((val, index) => (
-                  <button
-                    key={index}
-                    className={
-                      val === "Content MGMT"
-                        ? "btn-type me-1 mb-1 btn-pink"
-                        : val === "Setting Database"
-                        ? "btn-type me-1 mb-1 btn-red"
-                        : val === "Account MGMT"
-                        ? "btn-type me-1 mb-1 btn-blue"
-                        : val === "Employee MGMT"
-                        ? "btn-type me-1 mb-1 btn-sky"
-                        : val === "Service MGMT"
-                        ? "btn-type me-1 mb-1 btn-yellow"
-                        : val === "Data Analysis System"
-                        ? "btn-type me-1 mb-1 btn-violet"
-                        : val === "Purchasing System"
-                        ? "btn-type me-1 mb-1 btn-blue"
-                        : val === "Document MGMT"
-                        ? "btn-type me-1 mb-1 btn-purple"
-                        : val === "Shop online"
-                        ? "btn-type me-1 mb-1 btn-orange"
-                        : val === "Paint Collection System"
-                        ? "btn-type me-1 mb-1 btn-moon"
-                        : val === "Report Statistics" &&
-                          "btn-type me-1 mb-1 btn-moon"
-                    }
-                  >
-                    {val}
-                  </button>
-                ))}
+                <div>
+                  <h5 className="font-poppins portfoio-title my-3">
+                    {port.title}
+                  </h5>
+                  {port.type.map((val, index) => (
+                    <button
+                      key={index}
+                      className={
+                        val === "Content MGMT"
+                          ? "btn-type me-1 mb-1 btn-pink"
+                          : val === "Setting Database"
+                          ? "btn-type me-1 mb-1 btn-red"
+                          : val === "Account MGMT"
+                          ? "btn-type me-1 mb-1 btn-blue"
+                          : val === "Employee MGMT"
+                          ? "btn-type me-1 mb-1 btn-sky"
+                          : val === "Service MGMT"
+                          ? "btn-type me-1 mb-1 btn-yellow"
+                          : val === "Data Analysis System"
+                          ? "btn-type me-1 mb-1 btn-violet"
+                          : val === "Purchasing System"
+                          ? "btn-type me-1 mb-1 btn-blue"
+                          : val === "Document MGMT"
+                          ? "btn-type me-1 mb-1 btn-purple"
+                          : val === "Shop online"
+                          ? "btn-type me-1 mb-1 btn-orange"
+                          : val === "Paint Collection System"
+                          ? "btn-type me-1 mb-1 btn-moon"
+                          : val === "Report Statistics" &&
+                            "btn-type me-1 mb-1 btn-moon"
+                      }
+                    >
+                      {val}
+                    </button>
+                  ))}
+                </div>
               </div>
             </div>
           ))}
